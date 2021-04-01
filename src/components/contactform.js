e7514c708f15d62c36e24013c0bea0cbd2d0df73
mikethedevdad <floresmichael6@yahoo.com>
2021-03-31 20:04:16 -0500

    Beginning to work on form

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
 
-const ContactContainer = styled.div`
+const ContactContainer = styled.form`
     margin-left: 20%;
     justify-content: center;
     @media screen and (max-width: 600px) {
@@ -9,17 +9,43 @@ const ContactContainer = styled.div`
     }
 `;
 
+// const contactform = () => {
+//     return (
+//         <ContactContainer id="contactSection">
+//             <h1>Contact</h1>
+//             <p>
+//                 Form coming soon, BUT, feel free to email at <a href="mailto:michaelflores@hey.com">michaelflores@hey.com</a> until then.
+//             </p>
+//         </ContactContainer>
+//     )
+// };
+
 const contactform = () => {
     return (
         <ContactContainer id="contactSection">
-            <h1>Contact</h1>
-            <p>
-                Form coming soon, BUT, feel free to email at <a href="mailto:michaelflores@hey.com">michaelflores@hey.com</a> until then.
-            </p>
+            <form name="contact" method="POST" data-netlify="true">
+              <p>
+                <label>Your Name: <input type="text" name="name" /></label>   
+              </p>
+              <p>
+                <label>Your Email: <input type="email" name="email" /></label>
+              </p>
+              <p>
+                <label>Your Role: <select name="role[]" multiple>
+                  <option value="leader">Leader</option>
+                  <option value="follower">Follower</option>
+                </select></label>
+              </p>
+              <p>
+                <label>Message: <textarea name="message"></textarea></label>
+              </p>
+              <p>
+                <button type="submit">Send</button>
+              </p>
+          </form>
         </ContactContainer>
     )
 };
-
 
 
 export default contactform;
