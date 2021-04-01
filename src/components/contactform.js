21b9833997f99c1367a4e95ed6192e83d4f60a9b
miketheappdev <floresmichael6@yahoo.com>
2021-03-31 20:25:37 -0500

    Testing Form stuff

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -22,8 +22,11 @@ const ContactContainer = styled.form`
 
 const contactform = () => {
     return (
-        <ContactContainer id="contactSection">
-            <form name="contact" method="POST" data-netlify="true">
+      <>
+      <h3>Contact Me!</h3>
+      <p>Looking for some work to be done? Are you hiring? Tell me below!</p>
+        <ContactContainer id="contactSection" name="mikeContactForm" method="POST" data-netlify="true" action="/ThankYou">
+          <input type="hidden" name="form-name" value="mikeContactForm" />
               <p>
                 <label>Your Name: <input type="text" name="name" /></label>   
               </p>
@@ -31,19 +34,13 @@ const contactform = () => {
                 <label>Your Email: <input type="email" name="email" /></label>
               </p>
               <p>
-                <label>Your Role: <select name="role[]" multiple>
-                  <option value="leader">Leader</option>
-                  <option value="follower">Follower</option>
-                </select></label>
-              </p>
-              <p>
                 <label>Message: <textarea name="message"></textarea></label>
               </p>
               <p>
-                <button type="submit">Send</button>
+                <button type="submit">Submit</button>
               </p>
-          </form>
         </ContactContainer>
+        </>
     )
 };
 
---
+++ src/pages/ThankYou.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+function goHome() {
+    window.location = '/';
+}
+
+export default function ThankYou() {
+    return (
+        <Layout>
+        <div>
+            <h3>Thanks for Contacting!</h3>
+            <p>I'll try to reach back out as soon as I can.</p>
+        </div>
+        <button type="button" onClick={goHome()}>Back</button>
+
+        </Layout>
+    )
+}
